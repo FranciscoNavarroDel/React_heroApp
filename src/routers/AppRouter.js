14bddf150e13994636a540e9141f9bc8fb69c401
Francisco <f.nav.del@gmail.com>
2021-09-02 21:25:15 -0400

    probando otra solucion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, HashRouter } from "react-router-dom";
 
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
   return (
-    <Router>
+    <HashRouter basename="/">
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
           <Route path="/" component={DashboardRoutes} />
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 };
